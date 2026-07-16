05c7965f7671a0e16ec6b16587a0aed64edc30d3
69070106-cloud <69070106@kmitl.ac.th>
2026-07-17 03:23:12 +0700

    2

--- Problems/3020.py
+++ Problems/3020.py
@@ -3,3 +3,15 @@ PRICE = int(input())
 LID = int(input())
 N_PRICE = int(input())
 NUM = int(input())
+
+if LID > 0:
+    NUM_A = NUM - 1
+    NUM_B = NUM_A // LID
+    NUM_C = NUM_A - NUM_B
+    TOTAL = ((NUM_B * N_PRICE) + (NUM_C * PRICE)) + PRICE
+elif LID == 0 and N_PRICE > 0:
+    TOTAL = NUM * N_PRICE
+else:
+    TOTAL = NUM * PRICE
+
+print(TOTAL)
