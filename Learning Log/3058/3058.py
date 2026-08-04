1ca075c84602a7e66f726007d8a9bd64e0f3e952
69070106-cloud <69070106@kmitl.ac.th>
2026-08-04 09:42:01 +0700

    new2

--- Learning Log/3058/3058.py
+++ Learning Log/3058/3058.py
@@ -2,14 +2,14 @@
 A = int(input())
 B = int(input())
 GOAL = int(input())
-TOTAL_A = A * 1
+TOTAL_A = A
 TOTAL_B = B * 5
 GOAL_B = GOAL % 5
 if TOTAL_B >= GOAL and not GOAL_B:
     print("0")
 elif TOTAL_B >= GOAL and GOAL_B and GOAL_B <= TOTAL_A:
     print(GOAL % 5)
-elif TOTAL_B + TOTAL_A >= GOAL:
+elif TOTAL_B + TOTAL_A >= GOAL and GOAL - TOTAL_B > 0:
     print(GOAL - TOTAL_B)
 else:
     print("-1")
